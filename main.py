2d89a2d2fc116b2ef5f6679f49ad222fffa15b43
JETTY CHETAN <jettychetan524@gmail.com>
2026-05-28 20:18:26 +0530

    added apis

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    name: str
+    age: int
 
 app=FastAPI()
 app.add_middleware(
@@ -10,6 +15,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+lis=[]
+
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
+
+@app.post("/post_data")
+def post_data(data:Input):
+    lis.append(data.model_dump())
+    return {"message":"Data received successfully"}
+
+@app.get("/get_data")
+def get_data():
+    return {"data":lis}
